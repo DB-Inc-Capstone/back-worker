1eceea664993a8443fb00c043aaa40954b11b5a0
DevTae <dev.taehyeon.kim@gmail.com>
2024-04-20 23:46:30 +0900

    Update test codes

--- src/test/java/kr/co/dbinc/BackWorkerApplicationTests.java
+++ src/test/java/kr/co/dbinc/BackWorkerApplicationTests.java
@@ -3,7 +3,7 @@ package kr.co.dbinc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BackWorkerApplicationTests {
 
 	@Test
